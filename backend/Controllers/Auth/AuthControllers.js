35f7237bca566d5452380c8ba7ecf51ad247f9a5
Purnot <purnoy.cc@gmail.com>
2024-02-13 17:36:35 +0600

    added login logic

--- backend/Controllers/Auth/AuthControllers.js
+++ backend/Controllers/Auth/AuthControllers.js
@@ -34,7 +34,7 @@ const signUp = catchAsync(async (req, res) => {
             res.status(201).json({
                 _id: newUser._id,
                 fullName: newUser.fullName,
-                username: newUser.userame,
+                username: newUser.username,
                 profilePic: newUser.profilePic,
             });
         } else {
@@ -47,8 +47,29 @@ const signUp = catchAsync(async (req, res) => {
 });
 
 const login = catchAsync(async (req, res) => {
-    res.send("Login User");
-    console.log("Login User");
+    try {
+        const { username, password } = req.body;
+        const user = await UserMainModel.findOne({ username });
+        const isPasswordCorrect = await bcrypt.compare(
+            password,
+            user?.password || ""
+        );
+        if (!user || !isPasswordCorrect) {
+            return res
+                .status(400)
+                .json({ error: "Invalid username or password" });
+        }
+        generateTokenAndSetCookie(user._id, res);
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            username: user.username,
+            profilePic: user.profilePic,
+        });
+    } catch (error) {
+        console.log("Error in login controller", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
 });
 const logOut = catchAsync(async (req, res) => {
     res.send("Logout User");
